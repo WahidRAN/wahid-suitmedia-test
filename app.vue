07108c1b04ece7f089f4f5bcfe710596c7b4050b
wran <wahidrizqiakbarn@gmail.com>
2024-04-19 09:42:05 +0700

    head title

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Monito Wahid Site',
+  ogTitle: 'Monito Wahid Site',
+  description: 'This is test site for suitmedia, let me tell you all about it.',
+  ogDescription: 'This is test site for suitmedia, let me tell you all about it.',
+})
+</script>
+
 <template>
   <div>
     <NuxtLayout>
